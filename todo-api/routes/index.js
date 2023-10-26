1a8a8b659e9deee007840aa9ad63f3c2dca25ebd
carami <carami@nate.com>
2023-10-26 09:52:53 +0900

    add todo-api

---
+++ todo-api/routes/index.js
@@ -0,0 +1,46 @@
+var express = require("express");
+var router = express.Router();
+
+let todos = [
+  {
+    id: 3,
+    title: "Todo 3",
+    done: false,
+  },
+  {
+    id: 2,
+    title: "Todo 2",
+    done: false,
+  },
+  {
+    id: 1,
+    title: "Todo 1",
+    done: false,
+  },
+];
+
+/* GET home page. */
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "carami" });
+});
+router.get("/hello", function (req, res) {
+  res.render("index", { title: "hi express" });
+});
+
+router.get("/api/todos", function (req, res) {
+  res.status(200).json(todos);
+});
+
+router.get("/api/todos/:id", function (req, res) {
+  const id = parseInt(req.params.id);
+  const todo = todos.find((todo) => todo.id === id);
+  res.status(200).json(todo);
+});
+let idGen = 3;
+router.post("/api/todos", function (req, res) {
+  const title = req.body.title;
+  const todo = { id: ++idGen, title: title, done: false };
+  todos.unshift(todo);
+  res.status(200).json(todo);
+});
+module.exports = router;
